550fd2598d3dc8a7f4b068d1fa7267549e767f93
Ravana <47528708+ravana69@users.noreply.github.com>
2019-11-08 13:53:19 +0530

    Update F.py

--- stdplugins/F.py
+++ stdplugins/F.py
@@ -3,9 +3,11 @@ import asyncio
 import os
 import sys
 from uniborg import util
+from telethon import events, functions, __version__
+from uniborg.util import admin_cmd
 
 
-@borg.on(util.admin_cmd("f ?(.*)"))
+@borg.on(admin_cmd(pattern="f"))
 async def payf(event):
     paytext=event.pattern_match.group(1)
     pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(paytext*8, paytext*8, paytext*2, paytext*2, paytext*2, paytext*6, paytext*6, paytext*2, paytext*2, paytext*2, paytext*2, paytext*2)
