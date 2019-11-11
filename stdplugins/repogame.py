9caedd1f5044777acba4336d786abec4022dc0b7
Ravana <47528708+ravana69@users.noreply.github.com>
2019-11-11 13:06:33 +0530

    Update repogame.py

--- stdplugins/repogame.py
+++ stdplugins/repogame.py
@@ -1,39 +1,22 @@
 """Emoji
-
 Available Commands:
 click gift as soon as fast as possible
 .game
-
 build by legend @r4v4n4 , if u edit it then u r gay..."""
-
 from telethon import events
-
 import asyncio
 
-
-
-
-
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
 async def _(event):
-
     if event.fwd_from:
-
         return
-
     animation_interval = 0.5
-
     animation_ttl = range(0, 14)
-
     input_str = event.pattern_match.group(1)
-
     if input_str == "game":
-
         await event.edit(input_str)
-
         animation_chars = [
-        
+       
             "**Welcome To Pornhub Repo Game**",
             "**Click The Gift As Fast As Possible**",
             "**Game Starts in 3**",
@@ -49,9 +32,6 @@ async def _(event):
             "🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆\n🎆🎇🎆🎇🎆🎇🎆\n🎇🎆🎇🎆🎇🎆🎇\n🎆🎇🎆🎇🎆🎇\n🎇🎆🎇🎆🎇🎆🎇",
             "**Game Over**"
  ]
-
         for i in animation_ttl:
-
             await asyncio.sleep(animation_interval)
-
             await event.edit(animation_chars[i % 14])
