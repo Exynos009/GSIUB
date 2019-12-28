2eb9772cddc37c399a3fb67ae337bff36ef40b56
Ravana <47528708+ravana69@users.noreply.github.com>
2019-12-28 23:16:03 +0530

    Update execmod.py

--- stdplugins/execmod.py
+++ stdplugins/execmod.py
@@ -1,4 +1,4 @@
-"""COMMAND : .cpu, .uptime, .suicide, .env, .pip, .neofetch, .coffeehouse, .date, .stdplugins, .fast, .iwantsex, .telegram, .listpip, .pyfiglet, .kowsay, .name, .faast, .daddyjoke, .fortune, .quote, .fakeid"""
+"""COMMAND : .cpu, .uptime, .suicide, .env, .pip, .neofetch, .coffeehouse, .date, .stdplugins, .fast, .iwantsex, .telegram, .listpip, .pyfiglet, .kowsay, .name, .faast, .daddyjoke, .fortune, .quote, .fakeid, .vpn, .kwot, .programming"""
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -907,6 +907,126 @@ async def _(event):
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
     OUTPUT = f"**[Ravana's](tg://need_update_for_some_feature/) , fake id generator for Your Pornhub...**\n"
+    stdout, stderr = await process.communicate()
+    if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(stdout)) as out_file:
+            out_file.name = "exec.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=OUTPUT,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    if stderr.decode():
+        await event.edit(f"**{stderr.decode()}**")
+        return
+    await event.edit(f"{OUTPUT}`{stdout.decode()}`")
+#    else:
+#        await event.edit("Unknown Command")
+
+
+@borg.on(admin_cmd(pattern="vpn"))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+#    dirname = event.pattern_match.group(1)
+#    tempdir = "localdir"
+    cmd = "aovpn"
+#    if dirname == tempdir:
+	
+    eply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    OUTPUT = f"**[Ravana's](tg://need_update_for_some_feature/) , open vpn for Your Pornhub...**\n"
+    stdout, stderr = await process.communicate()
+    if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(stdout)) as out_file:
+            out_file.name = "exec.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=OUTPUT,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    if stderr.decode():
+        await event.edit(f"**{stderr.decode()}**")
+        return
+    await event.edit(f"{OUTPUT}`{stdout.decode()}`")
+#    else:
+#        await event.edit("Unknown Command")
+
+
+@borg.on(admin_cmd(pattern="kwot"))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+#    dirname = event.pattern_match.group(1)
+#    tempdir = "localdir"
+    cmd = "kwot 5"
+#    if dirname == tempdir:
+	
+    eply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    OUTPUT = f"**[Ravana's](tg://need_update_for_some_feature/) , kwot for Your Pornhub...**\n"
+    stdout, stderr = await process.communicate()
+    if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
+        with io.BytesIO(str.encode(stdout)) as out_file:
+            out_file.name = "exec.text"
+            await borg.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=OUTPUT,
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    if stderr.decode():
+        await event.edit(f"**{stderr.decode()}**")
+        return
+    await event.edit(f"{OUTPUT}`{stdout.decode()}`")
+#    else:
+#        await event.edit("Unknown Command")
+
+
+@borg.on(admin_cmd(pattern="programming"))
+async def _(event):
+    if event.fwd_from:
+        return
+    DELAY_BETWEEN_EDITS = 0.3
+    PROCESS_RUN_TIME = 100
+#    dirname = event.pattern_match.group(1)
+#    tempdir = "localdir"
+    cmd = "programmingquotes -l EN"
+#    if dirname == tempdir:
+	
+    eply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    start_time = time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    OUTPUT = f"**[Ravana's](tg://need_update_for_some_feature/) , programming quotes for Your Pornhub...**\n"
     stdout, stderr = await process.communicate()
     if len(stdout) > Config.MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(stdout)) as out_file:
