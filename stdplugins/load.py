7d559f913ffa1d9730b8da5d7ed2f7bf59e115a3
Ravana <47528708+ravana69@users.noreply.github.com>
2020-01-03 00:00:17 +0530

    Update load.py

--- stdplugins/load.py
+++ stdplugins/load.py
@@ -7,7 +7,8 @@ Available Commands:
 .up
 .round
 .heart
-.anim"""
+.anim
+.fnl"""
 
 from telethon import events
 
@@ -278,3 +279,44 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 11])
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 6)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "fnl":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "😁🏿",
+
+            "😁🏾",
+
+            "😁🏽",
+
+            "😁🏼",
+
+            "‎😁",
+
+            "__**Fair & Lovely GeNg Is BeHiNd You....**__"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 6])
