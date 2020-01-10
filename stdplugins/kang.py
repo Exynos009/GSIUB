60ec283b5f0543d5206f5e1bd5aa2b0d9c056482
Ravana <47528708+ravana69@users.noreply.github.com>
2020-01-10 08:04:10 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -45,7 +45,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"@r4v4n4 geng beng"
-    packshortname = f"ravana_gengbeng{userid}"  # format: Uni_Borg_userid
+    packshortname = f"ravana_gengbeng01{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "ravana_Sticker.png"
