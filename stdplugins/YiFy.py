4e618286e2684193c3087ac8117663e776ec8734
Ravana <47528708+ravana69@users.noreply.github.com>
2020-01-19 18:18:17 +0530

    Update YiFy.py

--- stdplugins/YiFy.py
+++ stdplugins/YiFy.py
@@ -9,16 +9,16 @@ from bs4 import BeautifulSoup
 import requests
 
 
-@borg.on(events.NewMessage(pattern=r"\.yify recents", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.dvd recents", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
     uploadbot = await borg.get_entity("@uploadbot")
-    BASE_URL = "https://yts.pm"
-    tg_feed_link = BASE_URL + "/browse-movies"
+    BASE_URL = "https://dvdwo.pw/"
+    tg_feed_link = BASE_URL + "/index.php?/forum/6-malayalam-new-dvdrips-hdrips-bdrips-movies/"
     main_page_response = requests.get(tg_feed_link)
     main_soup = BeautifulSoup(main_page_response.text, "html.parser")
-    movies_in_page = main_soup.find_all("div", class_="browse-movie-wrap")
+    movies_in_page = main_soup.find_all("div", class_="index.php?/forum/6-malayalam-new-dvdrips-hdrips-bdrips-movies-wrap")
     for movie in movies_in_page:
         movie_bottom = movie.div
         name = movie_bottom.a.string
