657a87da476f49ba8dabd2623d00f2b9ba9797c2
Ravana <47528708+ravana69@users.noreply.github.com>
2020-01-23 20:06:25 +0530

    Update and rename load.py to animate.py

--- stdplugins/animate.py
+++ stdplugins/animate.py
@@ -320,3 +320,44 @@ async def _(event):
             await asyncio.sleep(animation_interval)
 
             await event.edit(animation_chars[i % 6])
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 2
+
+    animation_ttl = range(0, 6)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "monkey":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "🐵",
+
+            "🙉",
+
+            "🙈",
+
+            "🙊",
+
+            "🖕‎🐵🖕",
+
+            "**Harami GeNg Is BeHiNd You....**"
+
+        ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 6])
