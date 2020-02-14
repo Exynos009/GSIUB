7d304e2f7fabfa6ba53875e8e224da14b4c343f2
Ravana <47528708+ravana69@users.noreply.github.com>
2020-02-14 21:27:20 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -2,7 +2,9 @@
 Available Commands:
 .lund [Optional Emoji]
 .packinfo
-.loda {for zip sticker pack}"""
+.loda {for zip sticker pack}
+
+edit packshortname to change pack name after 120 stickers"""
 from telethon import events
 from io import BytesIO
 from PIL import Image
@@ -45,7 +47,7 @@ async def _(event):
     me = borg.me
     userid = event.from_id
     packname = f"@r4v4n4 geng beng"
-    packshortname = f"ravana_gengbeng02{userid}"  # format: Uni_Borg_userid
+    packshortname = f"ravana_gengbeng03{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "ravana_Sticker.png"
