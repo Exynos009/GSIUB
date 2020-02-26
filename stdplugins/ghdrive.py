19d80702c0660809e6fcaeb3befbdc116b5fa698
Ravana <47528708+ravana69@users.noreply.github.com>
2020-02-26 12:00:59 +0530

    Create ghdrive.py

---
+++ stdplugins/ghdrive.py
@@ -0,0 +1,112 @@
+"""Upload local Files to gDrive
+Syntax:
+.ghdrive (does not show link)
+ """
+
+# The entire code given below is verbatim copied from
+# https://github.com/cyberboysumanjay/Gdrivedownloader/blob/master/gdrive_upload.py
+# there might be some changes made to suit the needs for this repository
+# Licensed under MIT License
+
+import asyncio
+import json
+import math
+import os
+import time
+from datetime import datetime
+from telethon import events
+from uniborg.util import admin_cmd, progress, humanbytes
+#
+from mimetypes import guess_type
+from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from apiclient.errors import ResumableUploadError
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.file import Storage
+from oauth2client import file, client, tools
+import httplib2
+
+
+# Path to token json file, it should be in same directory as script
+G_DRIVE_TOKEN_FILE = Config.TMP_DOWNLOAD_DIRECTORY + "/auth_token.txt"
+# Copy your credentials from the APIs Console
+CLIENT_ID = Config.G_DRIVE_CLIENT_ID
+CLIENT_SECRET = Config.G_DRIVE_CLIENT_SECRET
+# Check https://developers.google.com/drive/scopes for all available scopes
+OAUTH_SCOPE = "https://www.googleapis.com/auth/drive.file"
+# Redirect URI for installed apps, can be left as is
+REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
+# global variable to set Folder ID to upload to
+G_DRIVE_F_PARENT_ID = None
+# global variable to indicate mimeType of directories in gDrive
+G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
+
+
+@borg.on(admin_cmd(pattern="ghdrive ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    mone = await event.reply("Processing hidden G-Drive Download...")
+    if CLIENT_ID is None or CLIENT_SECRET is None:
+        await mone.edit("This module requires credentials from https://da.gd/so63O. Aborting!")
+        return
+    if Config.PRIVATE_GROUP_BOT_API_ID is None:
+        await event.edit("Please set the required environment variable `PRIVATE_GROUP_BOT_API_ID` for this plugin to work")
+        return
+    input_str = event.pattern_match.group(1)
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    required_file_name = None
+    start = datetime.now()
+    if event.reply_to_msg_id and not input_str:
+        reply_message = await event.get_reply_message()
+        try:
+            c_time = time.time()
+            downloaded_file_name = await borg.download_media(
+                reply_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, mone, c_time, "trying to download")
+                )
+            )
+        except Exception as e:  # pylint:disable=C0103,W0703
+            await mone.edit(str(e))
+            return False
+        else:
+            end = datetime.now()
+            ms = (end - start).seconds
+            required_file_name = downloaded_file_name
+            await mone.edit("Downloaded to `{}` in {} seconds.".format(downloaded_file_name, ms))
+    elif input_str:
+        input_str = input_str.strip()
+        if os.path.exists(input_str):
+            end = datetime.now()
+            ms = (end - start).seconds
+            required_file_name = input_str
+            await mone.edit("Found `{}` in {} seconds.".format(input_str, ms))
+        else:
+            await mone.edit("File Not found in local server. Give me a file path :((")
+            return False
+    # logger.info(required_file_name)
+    if required_file_name:
+        #
+        if Config.AUTH_TOKEN_DATA is not None:
+            with open(G_DRIVE_TOKEN_FILE, "w") as t_file:
+                t_file.write(Config.AUTH_TOKEN_DATA)
+        # Check if token file exists, if not create it by requesting authorization code
+        storage = None
+        if not os.path.isfile(G_DRIVE_TOKEN_FILE):
+            storage = await create_token_file(G_DRIVE_TOKEN_FILE, event)
+        http = authorize(G_DRIVE_TOKEN_FILE, storage)
+        # Authorize, get file parameters, upload file and print out result URL for download
+        # http = authorize(G_DRIVE_TOKEN_FILE, None)
+        file_name, mime_type = file_ops(required_file_name)
+        # required_file_name will have the full path
+        # Sometimes API fails to retrieve starting URI, we wrap it.
+        try:
+            g_drive_link = await upload_file(http, required_file_name, file_name, mime_type, mone, G_DRIVE_F_PARENT_ID)
+            await mone.edit(f"**🔥Encrypted G-Drive🔥** \n File Name: {file_name} \nHere is your Google Drive link: **Hidden Link**")
+        except Exception as e:
+            await mone.edit(f"Exception occurred while uploading to gDrive {e}")
+    else:
+        await mone.edit("File Not found in local server. Give me a file path :((")
