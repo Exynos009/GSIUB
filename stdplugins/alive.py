beeac2c1185014764e530cc3ae3c540ef36982f3
Ravana <47528708+ravana69@users.noreply.github.com>
2020-03-02 15:06:57 +0530

    Update alive.py

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -9,7 +9,7 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "`Your bot is running\n\nTelethon version: 1.10.10\nPython: 3.7.4\nUser: @r4v4n4\nDatabase Status: Telegram Databases functioning normally!`"
+    mentions = "`Your bot is running\n\nTelethon version: 1.11.3\nPython: 3.8.2\nUser: @r4v4n4\nDatabase Status: Telegram Databases functioning normally!`"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
         mentions += f""
