8822e038be20338152e48ab7272b247ff0dc1424
Ravana <47528708+ravana69@users.noreply.github.com>
2020-04-30 15:37:47 +0530

    Update helpme.py

--- stdplugins/helpme.py
+++ stdplugins/helpme.py
@@ -14,7 +14,7 @@ async def _(event):
     if event.fwd_from:
         return
     if Config.USER is not None:
-        user = f"\n```User: {Config.USER}```"
+        user = f"\n```User: {Config.user}```"
     else:
         user = "RAVANA @tharkigang"
     uname = platform.uname()
