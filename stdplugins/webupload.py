da29bf25e81229ff751f98e4c7e34eee044dac2d
Ravana <47528708+ravana69@users.noreply.github.com>
2020-05-09 10:12:05 +0530

    Update webupload.py

--- stdplugins/webupload.py
+++ stdplugins/webupload.py
@@ -9,7 +9,7 @@ import json
 import os
 
 
-@borg.on(admin_cmd(pattern="webupload ?(.+?|) --(fileinfo|fileio|oload|anonfiles|transfer|filebin|anonymousfiles|megaupload|bayfiles)"))
+@borg.on(admin_cmd(pattern="webupload ?(.+?|) --(mixdrop|fileinfo|fileio|oload|anonfiles|transfer|filebin|anonymousfiles|megaupload|bayfiles)"))
 async def _(event):
 	await event.edit("processing ...")
 	PROCESS_RUN_TIME = 100
@@ -25,6 +25,7 @@ async def _(event):
 		)
 	# a dictionary containing the shell commands
 	CMD_WEB = {
+                "mixdrop": "curl -X POST -F 'email=1996ravana@gmail.com' -F 'key=W7iGTx6LONW58TqRAzd6' -F 'file=@{full_file_path}' https://ul.mixdrop.co/api",
                 "fileinfo": "ffmpeg -i \"file=@{full_file_path}\"",
                 "fileio": "curl -F \"file=@{full_file_path}\" https://file.io",
                 "oloda": "curl -F \"file=@{full_file_path}\" https://api.openload.cc/upload?token=ac0c09b75f63054c",
