f1625a2f9bdc5a7b8c86638f89cad2df6cf4c6c7
Ravana <47528708+ravana69@users.noreply.github.com>
2020-05-12 20:49:54 +0530

    Update Bloom.py

--- stdplugins/Bloom.py
+++ stdplugins/Bloom.py
@@ -55,7 +55,7 @@ async def autopic(event):
         current_time = datetime.now().strftime("\n Time: %H:%M:%S \n \n Date: %d/%m/%y")
         img = Image.open(photo)
         drawn_text = ImageDraw.Draw(img)
-        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 60)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 100)
         ofnt = ImageFont.truetype(FONT_FILE_TO_USE, 250)
         drawn_text.text((200, 400), current_time, font=fnt, fill=(FR,FG,FB))
         drawn_text.text((250, 250), "      😈", font = ofnt, fill=(FR,FG,FB))
